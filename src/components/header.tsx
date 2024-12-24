1f8af5cbe2eb4776545a63a49dc6943ba1f90605
farhan-navas <farhanmnavas@gmail.com>
2024-12-24 19:06:31 +0800

    main page ui fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,8 +3,7 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { ModelDropdown } from "./model-dropdown";
 import { SidebarTrigger } from "./ui/sidebar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "./ui/button";
 
 export function Header({
   children,
@@ -12,21 +11,20 @@ export function Header({
 }: Readonly<{ children: React.ReactNode; state: string }>) {
   return (
     <main style={{ flexGrow: 1 }}>
-      <div className="fixed top-0 flex w-full items-center justify-between p-4">
+      <div className="flex w-full items-center justify-between space-x-4 p-4">
         <div className="flex items-center space-x-4">
           {state === "collapsed" && <SidebarTrigger />}
           <ModelDropdown />
         </div>
-        <div className="flex items-center space-x-4">
+        <div className="space-x-4">
           <SignedOut>
-            <SignInButton />
+            <SignInButton>
+              <Button variant="blue">Sign In</Button>
+            </SignInButton>
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
-        </div>
-        <div className="flex items-center space-x-4">
-          <FontAwesomeIcon icon={faCircleUser} color="black" />
         </div>
       </div>
       <div className="mt-4">{children}</div>
