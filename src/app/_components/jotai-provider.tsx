331a358ed080f815b4c70c18a9033b3f984af73e
farhan-navas <farhanmnavas@gmail.com>
2025-05-07 16:29:13 +0800

    use sessionstorage instead of local

--- src/app/_components/jotai-provider.tsx
+++ src/app/_components/jotai-provider.tsx
@@ -1,9 +1,18 @@
 "use client";
 
-import { Provider } from "jotai";
+import { useUser } from "@clerk/nextjs";
+import { createStore, Provider } from "jotai";
+import { useMemo } from "react";
 
 export default function JotaiProvider({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <Provider>{children}</Provider>;
+  const user = useUser();
+  const myStore = useMemo(() => {
+    console.log("Creating new store for ", user.user);
+    return createStore();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user.user?.id]);
+
+  return <Provider store={myStore}>{children}</Provider>;
 }
